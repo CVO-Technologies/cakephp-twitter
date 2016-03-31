7c6dca67c709515748d4cecc8d12f2c6823e7b05
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-03-31 21:16:01 +0200

    Add a notifier transport for the Notifier plugin

---
+++ src/Notifier/Transport/TwitterTransport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CvoTechnologies\Twitter\Notifier\Transport;
+
+use Cake\Datasource\ModelAwareTrait;
+use CvoTechnologies\Notifier\AbstractTransport;
+use CvoTechnologies\Notifier\Notification;
+
+/**
+ * @property \CvoTechnologies\Twitter\Model\Endpoint\StatusesEndpoint Statuses
+ */
+class TwitterTransport extends AbstractTransport
+{
+    use ModelAwareTrait;
+
+    const TYPE = 'twitter';
+
+    /**
+     * Send notification
+     *
+     * @param \CvoTechnologies\Notifier\Notification $notification Notification instance.
+     * @return array
+     */
+    public function send(Notification $notification)
+    {
+        $this->modelFactory('Endpoint', ['Muffin\Webservice\Model\EndpointRegistry', 'get']);
+        $this->loadModel('CvoTechnologies/Twitter.Statuses', 'Endpoint');
+
+        $status = $this->Statuses->save($this->Statuses->newEntity([
+            'text' => $notification->message(static::TYPE)
+        ]));
+        if (!$status) {
+            return false;
+        }
+
+        return $status->toArray();
+    }
+}
