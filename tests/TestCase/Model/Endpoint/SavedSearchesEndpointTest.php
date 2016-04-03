f4963757bcb606963540d199d1e84a4a9ba1fa5d
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-04-03 21:26:54 +0200

    Add tests for saved searches endpoint

---
+++ tests/TestCase/Model/Endpoint/SavedSearchesEndpointTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CvoTechnologies\Twitter\Test\TestCase\Model\Endpoint;
+
+use Cake\Datasource\ConnectionManager;
+use Cake\TestSuite\TestCase;
+use Cake\Validation\Validator;
+use CvoTechnologies\StreamEmulation\Emulation\HttpEmulation;
+use CvoTechnologies\StreamEmulation\StreamWrapper;
+use GuzzleHttp\Psr7\Response;
+use Muffin\Webservice\Model\EndpointRegistry;
+
+class SavedSearchesEndpointTest extends TestCase
+{
+    public function setUp()
+    {
+        StreamWrapper::overrideWrapper('https');
+        ConnectionManager::config('twitter', [
+            'className' => 'Muffin\Webservice\Connection',
+            'service' => 'CvoTechnologies/Twitter.Twitter'
+        ]);
+    }
+
+    public function testInitialize()
+    {
+        $listsEndpoint = EndpointRegistry::get('CvoTechnologies/Twitter.SavedSearches');
+
+        $this->assertEquals('id', $listsEndpoint->primaryKey());
+        $this->assertEquals('name', $listsEndpoint->displayField());
+    }
+
+    public function testBuildRules()
+    {
+        $listsEndpoint = EndpointRegistry::get('CvoTechnologies/Twitter.SavedSearches');
+        $rulesChecker = $this->getMockBuilder('Cake\ORM\RulesChecker')
+            ->setMethods(['addCreate'])
+            ->getMock();
+        $rulesChecker->expects($this->once())
+            ->method('addCreate');
+        $listsEndpoint->buildRules($rulesChecker);
+    }
+
+    public function testCreate()
+    {
+        StreamWrapper::emulate(HttpEmulation::fromCallable(function () {
+            return new Response(200, [], json_encode([
+                'id' => 1,
+            ]));
+        }));
+
+        $listsEndpoint = EndpointRegistry::get('CvoTechnologies/Twitter.SavedSearches');
+        $list = $listsEndpoint->newEntity([
+        ]);
+        $this->assertInstanceOf('Muffin\Webservice\Model\Resource', $listsEndpoint->save($list));
+    }
+
+    public function tearDown()
+    {
+        ConnectionManager::drop('twitter');
+        StreamWrapper::restoreWrapper('https');
+    }
+}
